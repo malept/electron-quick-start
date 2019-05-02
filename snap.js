f54f544c5a0ecd6e3b3a0effce55626dc99c6313
Mark Lee <electronjs@lazymalevolence.com>
2019-05-02 16:35:50 -0700

    alsa

--- snap.js
+++ snap.js
@@ -4,7 +4,7 @@ const packager = require('electron-packager')
 const snap = require('electron-installer-snap')
 
 packager({ dir: '.', overwrite: true, out: '/root/out', tmpdir: false })
-  .then(paths => snap({ src: paths[0], dest: '/root/snap', features: { audio: true } }))
+  .then(paths => snap({ src: paths[0], dest: '/root/snap', features: { alsa: true } }))
   .then(snapPath => console.log(`Your generated Snap is at: ${snapPath}`))
   .catch(err => {
     console.error(`FAIL: ${err.message || err}`)
