5da5ac6c491bb5e7982a887acdbc2f2d9b0cfea8
Mark Lee <electronjs@lazymalevolence.com>
2019-05-02 14:03:14 -0700

    Fix out path

--- snap.js
+++ snap.js
@@ -3,5 +3,5 @@
 const packager = require('electron-packager')
 const snap = require('electron-installer-snap')
 
-packager({ dir: '.', overwrite: true, out: '/tmp/packager' })
+packager({ dir: '.', overwrite: true, out: '/packager' })
   .then(paths => snap({ src: paths[0] }))
