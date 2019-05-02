2c04277b197c6f4e0df8be2ba1bbccb8669f7cfd
Mark Lee <electronjs@lazymalevolence.com>
2019-05-02 14:14:40 -0700

    Print snap path

--- snap.js
+++ snap.js
@@ -5,6 +5,7 @@ const snap = require('electron-installer-snap')
 
 packager({ dir: '.', overwrite: true, out: '/root/out', tmpdir: false })
   .then(paths => snap({ src: paths[0] }))
+  .then(snapPath => console.log(`Your generated Snap is at: ${snapPath}`))
   .catch(err => {
     console.error(`FAIL: ${err.message || err}`)
     process.exit(1)
