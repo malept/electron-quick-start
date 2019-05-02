791e1cf573b652d4e20872bec68a4fbdaa999d0d
Mark Lee <electronjs@lazymalevolence.com>
2019-05-02 14:09:30 -0700

    Better fail

--- snap.js
+++ snap.js
@@ -5,3 +5,7 @@ const snap = require('electron-installer-snap')
 
 packager({ dir: '.', overwrite: true, out: '/root/out', tmpdir: false })
   .then(paths => snap({ src: paths[0] }))
+  .catch(err => {
+    console.error(`FAIL: ${err.message || err}`)
+    process.exit(1)
+  })
