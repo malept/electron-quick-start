2e4aafaedd84192baab539dabf901f956f75336b
Mark Lee <electronjs@lazymalevolence.com>
2020-12-28 17:47:28 -0800

    Improvements

--- verify-ignored.js
+++ verify-ignored.js
@@ -4,15 +4,25 @@ const fs = require('fs')
 const path = require('path')
 
 const outDir = path.resolve(__dirname, 'out', `electron-quick-start-${process.platform}-${process.arch}`)
-const appDir = path.join(outDir, 'resources', 'app')
 
-if (fs.existsSync(path.join(appDir, 'test'))) {
-  console.error('ERROR: Root test/ folder should not exist')
+function generateAppDir() {
+  if (process.platform === 'darwin') {
+    return path.join(outDir, 'electron-quick-start.app', 'Contents', 'Resources', 'app')
+  } else {
+    return path.join(outDir, 'resources', 'app')
+  }
+}
+
+const appDir = generateAppDir()
+const root = path.join(appDir, 'test')
+if (fs.existsSync()) {
+  console.error('ERROR: Root test/ folder should not exist': root)
   process.exit(1)
 }
 
-if (!fs.existsSync(path.join(appDir, 'foo', 'bar', 'test'))) {
-  console.error('ERROR: nested test folder should exist')
+const nested = path.join(appDir, 'foo', 'bar', 'test')
+if (!fs.existsSync(nested)) {
+  console.error('ERROR: nested test folder should exist:', nested)
   process.exit(1)
 }
 
